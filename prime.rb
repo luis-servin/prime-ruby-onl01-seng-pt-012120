abe550776a4a1d8ead7615f511577ed0140eb0aa
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 18:16:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,11 @@
 def prime?
   i = 2 
   if number > 1 
-    range = (i..number-1).t# Add  code here!
+    range = (i..number-1).to_a 
+    range.none? do |num_to_test|
+      number% num_to_test == 0 
+    end 
+  else 
+    false
+  end 
+end 
